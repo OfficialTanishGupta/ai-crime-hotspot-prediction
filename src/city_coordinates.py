80f15f833f40012a5fa9448affb5cb536f301375
OfficialTanishGupta <officialtanishgupta@gmail.com>
2026-01-07 22:30:36 +0530

    Initial commit with notebook and dataset structure

---
+++ src/city_coordinates.py
@@ -0,0 +1,54 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "18edb3a9-a138-47d5-ae34-9baf7403abd1",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "CITY_COORDINATES = {\n",
+    "    \"Delhi\": (28.6139, 77.2090),\n",
+    "    \"Mumbai\": (19.0760, 72.8777),\n",
+    "    \"Bengaluru\": (12.9716, 77.5946),\n",
+    "    \"Chennai\": (13.0827, 80.2707),\n",
+    "    \"Kolkata\": (22.5726, 88.3639),\n",
+    "    \"Hyderabad\": (17.3850, 78.4867),\n",
+    "    \"Pune\": (18.5204, 73.8567),\n",
+    "    \"Ahmedabad\": (23.0225, 72.5714),\n",
+    "    \"Jaipur\": (26.9124, 75.7873),\n",
+    "    \"Lucknow\": (26.8467, 80.9462)\n",
+    "}"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "64d08a82-6baf-4654-9d2d-6ad691230c00",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.14.0"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
